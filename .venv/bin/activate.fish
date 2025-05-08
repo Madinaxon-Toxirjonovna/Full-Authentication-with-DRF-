f73a0cf61468426a280a6c4bdc67ed07b2457203
Madinaxon Toxirjonovna <madinaxontoxirjonovna@gmail.com>
2025-05-08 21:20:00 +0500

    DRF

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/madinaxon/Desktop/Najot/TEST_AUTH/.venv
+set -gx VIRTUAL_ENV /home/madinaxon/Desktop/Najot/Full-Authentication-with-DRF-/.venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
